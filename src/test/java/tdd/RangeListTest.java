c1d4073471824cdb9c0ab2da85806bf0900760d5
RDV1KOR <RDV1KOR@BANI-C-00058.bmh.apac.bosch.com>
2021-05-07 02:57:26 +0530

    Fixed test class

--- src/test/java/tdd/RangeListTest.java
+++ src/test/java/tdd/RangeListTest.java
@@ -9,12 +9,6 @@ import java.util.List;
 
 import org.junit.Test;
 
-/**
- * Test class to test the functionality of the class {@link RangeList}
- * 
- * @author RDV1KOR
- *
- */
 public class RangeListTest
 {
   @Test
@@ -30,39 +24,6 @@ public class RangeListTest
   public void listIsNotEmptyThenDetectReadings()
   {
     List<Integer> currentSampleList = Arrays.asList(3, 3, 5, 4, 10, 11, 12);
-    RangeList rangeList = new RangeList();
-    rangeList.addAllRanges(currentSampleList);
-    assertFalse(rangeList.getAllRanges().isEmpty());
-    rangeList.detectRange();
-    assertFalse(rangeList.getDetectedRange().isEmpty());
-  }
-}
-package tdd;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.Test;
-
-public class RangeListTest
-{
-  @Test
-  public void listIsEmptyThenTrue()
-  {
-    List<Integer> currentSampleList = new ArrayList<>();
-    RangeList range = new RangeList();
-    range.addAllRanges(currentSampleList);
-    assertTrue(range.isEmpty());
-  }
-
-  @Test
-  public void listIsNotEmptyThenDetectReadings()
-  {
-    List<Integer> currentSampleList = Arrays.asList(3, 3, 5, 4, 7, 8, 10, 11, 12);
     RangeList rangeList = new RangeList();
     rangeList.addAllRanges(currentSampleList);
     assertFalse(rangeList.getAllRanges().isEmpty());
